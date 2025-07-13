dad5a51bd2ea7cd39a2de173069e617301ecc149
alexandra-n.palma <alexandra-n.palma@gcash.com>
2025-07-13 19:54:49 +0800

    Initial commit for Ticketing Module

--- src/main/java/org/example/service/TicketingModuleService.java
+++ src/main/java/org/example/service/TicketingModuleService.java
@@ -2,31 +2,64 @@ package org.example.service;
 
 import org.example.models.Ticket;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class TicketingModuleService {
-    static Random rand = new Random();
-    static Scanner sc = new Scanner(System.in);
-    static ArrayList<Ticket> ticketsList = new ArrayList<>();
+    private static Random rand = new Random();
+    private static Scanner sc = new Scanner(System.in);
+    public static ArrayList<Ticket> ticketsList = new ArrayList<>();
+    private static ArrayList<String> ticketNumbers = new ArrayList<>();
+    private static String choice;
 
-    static Path outputCsv = Paths.get("../resources/tickets.csv");
+    private static final int CHILD_PRICE = 0;
+    private static final int STUDENT_PRICE = 75;
+    private static final int ADULT_PRICE = 150;
+    private static final int SENIOR_PRICE = 50;
+
+    private static final String CHILD_DISCOUNT = "a FREE ENTRANCE";
+    private static final String STUDENT_DISCOUNT = "a STUDENT discount";
+    private static final String ADULT_NO_DISCOUNT = "a REGULAR ADULT ticket";
+    private static final String SENIOR_DISCOUNT = "a SENIOR discount";
+
+    static String fileName = "src/main/java/org/example/resources/tickets.csv";
 
     public static void main(String[] args) {
+
+        ticketsList.clear();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] record = line.split(",");
+                Ticket temp = new Ticket(record[0], record[1]);
+                ticketsList.add(temp);
+                ticketNumbers.add(record[1]);
+            }
+        } catch (IOException e) {
+            System.err.println("An error occurred while reading from the file: " + e.getMessage());
+        }
 
         System.out.println("=== WELCOME TO THE ZOO TICKET SHOP ===");
         System.out.println();
         System.out.println();
-        System.out.println("Would you like to buy a ticket? (1 = Yes | 2 = No)");
+        System.out.println("Would you like to buy a ticket? (Yes/No)");
         System.out.print(">> ");
 
-        try {
-            int choice = sc.nextInt();
+        initiatePurchase(false);
+    }
 
-            if (choice == 1) {
+    private static void initiatePurchase(boolean repeatPurchase) {
+        try {
+            if (repeatPurchase) {
+                choice = "yes";
+            } else {
+                choice = sc.next();
+            }
+
+            if (choice.equalsIgnoreCase("yes")) {
                 System.out.print("Enter your name: ");
                 String name = sc.next();
                 System.out.print("Enter your age: ");
@@ -35,22 +68,67 @@ public class TicketingModuleService {
                 int ageRange = validateAgeRange(age);
 
                 switch (ageRange) {
-                    case 1:
-                        System.out.println("You qualify for a free entrance!");
-                        System.out.println("Ticket price: Php 0.00");
-                        System.out.print("Would you like to proceed? (Yes/No): ");
-                        String purchaseChoice = sc.next();
-
-                        if (purchaseChoice.equalsIgnoreCase("yes")) {
-                            int ticketNumber = randomizeTicketNumber();
-                            System.out.println("Ticket purchased!");
-                            System.out.printf("Your ticket code is: ZOO-%d", ticketNumber);
-                        }
+                    case 1 -> proceedWithPurchase(name, CHILD_DISCOUNT, CHILD_PRICE);
+                    case 2 -> proceedWithPurchase(name, STUDENT_DISCOUNT, STUDENT_PRICE);
+                    case 3 -> proceedWithPurchase(name, ADULT_NO_DISCOUNT, ADULT_PRICE);
+                    case 4 -> proceedWithPurchase(name, SENIOR_DISCOUNT, SENIOR_PRICE);
+                    default -> System.out.println("Encountered problem with purchase. Please try again.");
                 }
+            } else if (choice.equalsIgnoreCase("no")) {
+                System.out.println("Hope to see you again some other time. Thank you and goodbye for now!");
+            } else {
+                System.out.println("Invalid choice. Please try again.");
+                System.out.println("Would you like to buy a ticket? (Yes/No)");
+                System.out.print(">> ");
+                initiatePurchase(false);
             }
         } catch (IllegalArgumentException e) {
-            System.out.println("Error: Invalid input. Please enter either 1 or 2 only.");
+            System.out.println("Error: Invalid input. Please enter either yes or no only.");
         }
+    }
+
+    private static void proceedWithPurchase(String name, String discountType, int price) {
+        System.out.printf("You qualify for %s!\n", discountType);
+        System.out.printf("Ticket price: Php %d.00\n", price);
+        System.out.print("Would you like to proceed? (Yes/No): ");
+        String purchaseChoice = sc.next();
+
+        if (purchaseChoice.equalsIgnoreCase("yes")) {
+            generateTicket(name);
+        } else {
+            System.out.println("See you next time, then!");
+        }
+    }
+
+    private static void generateTicket(String name) {
+        int ticketNumber = randomizeTicketNumber();
+
+        if (ticketNumbers.contains("ZOO-" + ticketNumber)) {
+            while (ticketNumbers.contains("ZOO-" + ticketNumber)) {
+                ticketNumber = randomizeTicketNumber();
+            }
+        }
+
+        System.out.println("Ticket purchased!");
+        System.out.printf("Your ticket code is: ZOO-%d", ticketNumber);
+
+        Ticket ticket = new Ticket(name, String.valueOf(ticketNumber));
+        ticketsList.add(ticket);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))){
+            writer.newLine();
+            writer.write(name + ",ZOO-" + ticketNumber);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("\n\n=== Ticket has been registered to system===");
+        System.out.println("Would you like to purchase another ticket? (Yes/No)");
+        System.out.print(">> ");
+        String repeatChoice = sc.next();
+
+        if (repeatChoice.equalsIgnoreCase("yes")) initiatePurchase(true);
+        else System.out.println("Thank you for using this service. See you soon!");
     }
 
     private static int validateAgeRange(int age) {
@@ -74,8 +152,6 @@ public class TicketingModuleService {
     }
 
     private static int randomizeTicketNumber() {
-        int ticketNumber = rand.nextInt(1000) + 1000;
-
-        return ticketNumber;
+        return rand.nextInt(1000) + 1000;
     }
 }
